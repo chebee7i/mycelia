5919d5bd8832225bd3e207549789128cad575296
chebee7i <chebee7i@gmail.com>
2012-03-07 20:06:17 -0800

    Correcting bad include statement.

--- src/tools/graphbuilder.cpp
+++ src/tools/graphbuilder.cpp
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <dataItem.hpp>
+#include "../dataitem.hpp"
 #include <tools/graphbuilder.hpp>
 
 using namespace std;
