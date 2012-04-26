570d2b7dfd48583641ad4ced7021b5dd2f0d570a
chebee7i <chebee7i@gmail.com>
2012-04-26 16:47:25 -0700

    Updated example.

--- python/demos/openGraph.py
+++ python/demos/openGraph.py
@@ -1,20 +1,27 @@
 import os
+import glob
 import time
-import xmlrpclib
+import itertools
 
-from nxwrapper import MyceliaGraph
+from mycelia import Graph
 
-g = MyceliaGraph()
+def take(iterable, n):
+    "Return first n items of the iterable as a list"
+    return list(itertools.islice(iterable, n))
+
+
+g = Graph()
 g.open_file('/home/ellisocj/dev/git/mycelia/data/actor100.xml')
 
-pwd = '/home/ellisocj/business/000/archive/metacore.ucdavis.edu/techno1/compounds/'
-x = os.listdir(pwd)
+image_dir = '/usr/share/pixmaps/'
+glb = '*.png'
+filenames = glob.glob(image_dir + glb)[:68]
+images = take(itertools.cycle(filenames), 100)
 
-time.sleep(3)
-for i,img in enumerate([x[0]]*100):
-    if img.endswith('png'):
-        z = pwd+img
-        g.server.set_node_type(i, 'image')
-        g.server.set_node_image_path(i, z)
+time.sleep(1)
+        
+for i,img in enumerate(images):
+    g.server.set_node_type(i, 'image')
+    g.server.set_node_image_path(i, img)
 
 
